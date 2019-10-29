d7163cfe08133d2337d6a9fffac30abf40466867
Huong NV <huongnv@tohsoft.com>
2019-10-29 17:23:17 +0700

    release tiếng việt

--- app/src/main/java/com/tohsoft/languagepack/vietnamese/MainActivity.java
+++ app/src/main/java/com/tohsoft/languagepack/vietnamese/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 
 
-import com.tohsoftkeyboard.languagepack.vietnamese.R;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -23,22 +22,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                TypedArray a = MainActivity.this.getResources().obtainTypedArray(R.array.vn_dict_array);
-//                try {
-//                   int[] resId = new int[a.length()];
-//                    for (int index = 0; index < a.length(); index++) {
-//                        resId[index] = a.getResourceId(index, 0);
-//                        loadDictionaryFromResource(resId);
-//                    }
-//                } finally {
-//                    a.recycle();
-//                }
-//            }
-//        }).start();
-
     }
     public static String convertStreamToString(InputStream is) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
@@ -51,48 +34,48 @@ public class MainActivity extends AppCompatActivity {
         reader.close();
         return sb.toString();
     }
-    private void loadDictionaryFromResource(int[] resId) {
-        final InputStream[] is = new InputStream[resId.length];
-        try {
-            // merging separated dictionary into one if dictionary is separated
-            int total = 0;
-            for (int i = 0; i < resId.length; i++) {
-                // http://ponystyle.com/blog/2010/03/26/dealing-with-asset-compression-in-android-apps/
-                // NOTE: the resource file can not be larger than 1MB
-                is[i] =getResources().openRawResource(resId[i]);
-                final int dictSize = is[i].available();
-                Log.d(
-                        TAG,
-                        "Will load a resource dictionary id "
-                                + resId[i]
-                                + " whose size is "
-                                + dictSize
-                                + " bytes.");
-                total += dictSize;
-                convertStreamToString(is[i]);
-            }
-
-
-        } catch (IOException e) {
-            Log.w(TAG, "No available memory for binary dictionary: " + e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            for (InputStream i1 : is) {
-                try {
-                    if (i1 != null) i1.close();
-                } catch (IOException e) {
-                    Log.w(TAG, "Failed to close input stream");
-                }
-            }
-        }
-    }
-    public static String getStringFromFile (String filePath) throws Exception {
-        File fl = new File(filePath);
-        FileInputStream fin = new FileInputStream(fl);
-        String ret = convertStreamToString(fin);
-        //Make sure you close all streams.
-        fin.close();
-        return ret;
-    }
+//    private void loadDictionaryFromResource(int[] resId) {
+//        final InputStream[] is = new InputStream[resId.length];
+//        try {
+//            // merging separated dictionary into one if dictionary is separated
+//            int total = 0;
+//            for (int i = 0; i < resId.length; i++) {
+//                // http://ponystyle.com/blog/2010/03/26/dealing-with-asset-compression-in-android-apps/
+//                // NOTE: the resource file can not be larger than 1MB
+//                is[i] =getResources().openRawResource(resId[i]);
+//                final int dictSize = is[i].available();
+//                Log.d(
+//                        TAG,
+//                        "Will load a resource dictionary id "
+//                                + resId[i]
+//                                + " whose size is "
+//                                + dictSize
+//                                + " bytes.");
+//                total += dictSize;
+//                convertStreamToString(is[i]);
+//            }
+//
+//
+//        } catch (IOException e) {
+//            Log.w(TAG, "No available memory for binary dictionary: " + e.getMessage());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            for (InputStream i1 : is) {
+//                try {
+//                    if (i1 != null) i1.close();
+//                } catch (IOException e) {
+//                    Log.w(TAG, "Failed to close input stream");
+//                }
+//            }
+//        }
+//    }
+//    public static String getStringFromFile (String filePath) throws Exception {
+//        File fl = new File(filePath);
+//        FileInputStream fin = new FileInputStream(fl);
+//        String ret = convertStreamToString(fin);
+//        //Make sure you close all streams.
+//        fin.close();
+//        return ret;
+//    }
 }
